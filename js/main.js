736a7bd647701df9b5e92b01cdbcc313aee0a65a
Aaryan Pashine <pashineaaryan@gmail.com>
2022-04-05 12:48:22 -0400

    Added Form and Weight and ability to add multiple contrasts

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
-let text1, text2, contrast, output;
+let text1, text2, contrast1, contrast2,contrast3, output;
 
 $("#generate").click(function(){
     
     text1 = $("#text01").val()
     text2 = $("#text02").val()
-    contrast = $("#contrast").find(":selected").text()
+    contrast1 = $("#contrast1").find(":selected").text()
+    contrast2 = $("#contrast2").find(":selected").text()
+    contrast3 = $("#contrast3").find(":selected").text()
 
 
+    output = `<div class="contrast--scale">
+    <h1 class="text01">${text1}</h1>
+    <h1 class="text02">${text2}</h1>
+    </div>`
 
-    if (contrast == "scale") {
+    $("#output").html(output)
+
+
+    if (contrast1 == "scale" || contrast2 == "scale" || contrast3 == "scale" ) {
 
         big = (Math.floor(Math.random()*100)+50)+"px"
-
-        output  = `<div class="contrast--scale">
-                    <h1 class="text01" style="font-size:${big}">${text1}</h1>
-                    <p class="text02">${text1}</p>
-                    </div>`
+        $(".text01").css({"font-size": big});
     
-    } else if (contrast == "direction"){
-
+    }if (contrast1 == "direction" || contrast2 == "direction" || contrast3 == "direction"){
+        
         rotate1 = "rotate("+(Math.floor(Math.random()*4)*45)+"deg)"
         rotate2 = "rotate("+(Math.floor(Math.random()*4)*45)+"deg)"
 
-        output  = `<div class="contrast--direction">
-                    <h3 class="text01" style="transform:${rotate1}">${text1}</h1>
-                    <h3 class="text02" style="transform:${rotate2}">${text2}</h1>
-                    </div>`
+        $(".text01").css({"transform":rotate1, "max-width":"fit-content"});
+        $(".text02").css({"transform":rotate2, "max-width":"fit-content"});
+        $("#output").css({"margin-top": "200px", "margin-left": "150px"});
 
+    }if (contrast1 == "weight" || contrast2 == "weight" || contrast3 == "weight"){
+        heavyWeights = [700, 800, 900];
+        heavy = heavyWeights[(Math.floor(Math.random()*3))%3];
+        lightWeights = [200, 300, 400, 500];
+        light = lightWeights[(Math.floor(Math.random()*4))%4];
+        $(".text01").css({"font-weight":heavy});
+        $(".text02").css({"font-weight":light});
+
+        
+    }if (contrast1 == "form" || contrast2 == "form" || contrast3 == "form"){
+       
+        if(contrast1 == "scale" || contrast2 == "scale" || contrast3 == "scale" ){
+            $(".text01").css({"text-transform":"uppercase"});
+        }else{       
+            size = parseInt($(".text01").css("font-size"))-2; 
+            $(".text01").css({"text-transform":"uppercase", "letter-spacing":"0.2em", "font-size": size+"px"});
+            $(".text02").css({"font-size": size-10+"px"});
+
+        }
+        
     }
 
-    console.log(output)
 
-    $("#output").html(output)
 
 
 })
